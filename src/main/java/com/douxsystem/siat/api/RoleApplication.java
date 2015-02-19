f1acad9fb7b87efa94a686aad668162ca217aee7
Santiago Hernandez <sslkeyregit@outlook.com>
2015-02-19 15:44:16 -0600

    Ember.js y REST
    Inicio de Web API REST y aplicacion Ember

---
+++ src/main/java/com/douxsystem/siat/api/RoleApplication.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.douxsystem.siat.api;
+
+import com.douxsystem.siat.domain.entities.Role;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Santiago
+ */
+@Path("/Roles")
+public class RoleApplication {
+
+    private static final String PERSISTENCE_UNIT_NAME = "SIAT_PersistanceUnit";
+    private static EntityManagerFactory factory;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject roles() {
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        JsonObject jsonb;
+        try {
+            List<Role> roles = em.createQuery("SELECT r FROM Role r").getResultList();
+            JsonArrayBuilder jsonab = Json.createArrayBuilder();
+            for (Role role : roles) {
+                JsonObjectBuilder jsonob = Json.createObjectBuilder();
+                jsonob.add("ID", role.getId());
+                jsonob.add("Nombre", role.getNombreRol());
+                jsonob.add("NivelAccesso", role.getNivelAccesso());
+                jsonab.add(jsonob);
+            }
+            jsonb = Json.createObjectBuilder().add("Status", true).add("Roles", jsonab).build();
+        } catch (Exception ex) {
+            jsonb = Json.createObjectBuilder().add("Status", false).add("Mensaje", ex.getMessage()).build();
+        } finally {
+            em.close();
+        }
+        return jsonb;
+    }
+}
--- src/main/java/com/douxsystem/siat/domain/entities/Role.java
+++ src/main/java/com/douxsystem/siat/domain/entities/Role.java
@@ -11,6 +11,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
@@ -21,6 +23,9 @@ import javax.xml.bind.annotation.XmlTransient;
  */
 @Entity
 @XmlRootElement
+@NamedQueries({
+    @NamedQuery(name="Role.findAll", query="SELECT r FROM Role r")
+})
 public class Role implements Serializable {
 
     private static final long serialVersionUID = 1L;
