91709fead8712156085470f08e92431df2864201
Santiago Hernandez <sslkeyregit@outlook.com>
2015-02-20 11:39:23 -0600

    Controladores Usuario
    Varios cambios en los controladores y la forma de despegar informacion

--- src/main/webapp/Scripts/script.js
+++ src/main/webapp/Scripts/script.js
@@ -19,12 +19,19 @@ siatApp.config(function ($routeProvider) {
             templateUrl: 'pages/user/home.html',
             controller: 'userHomeController'
         })
+        .when('/user/getTaxi', {
+            templateUrl: 'pages/user/getTaxi.html',
+            controller: 'usergetTaxiController'
+        })
 });
 
 // create the controller and inject Angular's $scope
 siatApp.controller('mainController', function ($scope) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
+    $scope.iniciarSesion = function () {
+        window.location.href = "#/user";
+    }
 });
 
 siatApp.controller('aboutController', function ($scope) {
@@ -33,5 +40,17 @@ siatApp.controller('aboutController', function ($scope) {
 
 siatApp.controller('userHomeController', function ($scope) {
     $scope.Usuario = 'Usuario Ejemplo',
-    $scope.personalData = {Nombre: 'Usuario Ejemplo', Domicilio:'Mariano Escobedo 124 Miguel Hidalgo DF'};
+    $scope.personalData = { Nombre: 'Usuario Ejemplo', Domicilio: 'Mariano Escobedo 124 Miguel Hidalgo DF' };
+});
+
+siatApp.controller('usergetTaxiController', function ($scope) {
+    $scope.solicitud = {
+        Nombre: 'Usuario Ejemplo',
+        Origen: 'Mariano Escobedo 124 Miguel Hidalgo DF',
+        Destino: null
+    }
+    $scope.getTaxiSuccess = function () {
+        alert('Se ha solicitado el taxi, debe de aparecer en su puerta en menos de 15 minutos');
+        window.location.href = "#/user";
+    }
 });
