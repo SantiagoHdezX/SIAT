49530e3468a6bdc634810bec2026bd5b0c63fbd0
Santiago Hernandez <sslkeyregit@outlook.com>
2015-02-22 23:41:40 -0600

    AngularJs: Vista y Controlador de Registro

--- src/main/webapp/Scripts/script.js
+++ src/main/webapp/Scripts/script.js
@@ -1,32 +1,35 @@
-﻿var siatApp = angular.module('siatApp', ['ngRoute']);
+var siatApp = angular.module('siatApp', ['ngRoute']);
 
 // configure our routes
 siatApp.config(function ($routeProvider) {
     $routeProvider
 
-        // route for the home page
-        .when('/', {
-            templateUrl: 'pages/home.html',
-            controller: 'mainController'
-        })
-
-        // route for the about page
-        .when('/about', {
-            templateUrl: 'pages/about.html',
-            controller: 'aboutController'
-        })
-        .when('/user', {
-            templateUrl: 'pages/user/home.html',
-            controller: 'userHomeController'
-        })
-        .when('/user/getTaxi', {
-            templateUrl: 'pages/user/getTaxi.html',
-            controller: 'usergetTaxiController'
-        })
-        .when('/admin', {
-            templateUrl: 'pages/admin/home.html',
-            controller: 'adminController'
-        })
+            // route for the home page
+            .when('/', {
+                templateUrl: 'Views/Home.html',
+                controller: 'mainController'
+            })
+            .when('/Register', {
+                templateUrl: 'Views/Register.html',
+                controller: 'RegisterCtrl'
+            })
+            // route for the about page
+            .when('/about', {
+                templateUrl: 'pages/about.html',
+                controller: 'aboutController'
+            })
+            .when('/user', {
+                templateUrl: 'pages/user/home.html',
+                controller: 'userHomeController'
+            })
+            .when('/user/getTaxi', {
+                templateUrl: 'pages/user/getTaxi.html',
+                controller: 'usergetTaxiController'
+            })
+            .when('/admin', {
+                templateUrl: 'pages/admin/home.html',
+                controller: 'adminController'
+            });
 });
 
 // create the controller and inject Angular's $scope
@@ -39,20 +42,38 @@ siatApp.controller('mainController', function ($scope) {
             window.location.href = "#/";
         }
         else {
-            if (login.admin === false) {
-                window.location.href = "#/user";
-            }
-            else {
-                window.location.href = "#/admin";
+            switch (parseInt(login.type.toString())) {
+                case 1:
+                    window.location.href = "#/User";
+                    break;
+                case 2:
+                    window.location.href = "#/TaxiDriver";
+                    break;
+                case 3:
+                    window.location.href = "#/Central";
+                    break;
+                case 4:
+                    window.location.href = "#/Directive";
+                    break;
             }
         }
-    }
+    };
+});
+
+siatApp.controller('RegisterCtrl', function ($scope) {
+    $scope.Register = {Address: {Estado: 'Distrito Federal'}};
+    $scope.RegistrarUsuario = function (Register) {
+        alert('Se ha registrado con exito');
+    };
+    $scope.Salir= function(){
+        window.location.href="#/";
+    };
 });
 
 siatApp.controller('adminController', function ($scope) {
     $scope.Usuario = 'Administrador Ejemplo';
-    $scope.personalData = { Nombre: 'Administrador Ejemplo', Domicilio: 'Domicilio Ejemplo' }
-})
+    $scope.personalData = {Nombre: 'Administrador Ejemplo', Domicilio: 'Domicilio Ejemplo'};
+});
 
 siatApp.controller('aboutController', function ($scope) {
     $scope.message = 'Aqui debe de ir la informacion correspondiente a la pagina "Acerca de"';
@@ -60,7 +81,7 @@ siatApp.controller('aboutController', function ($scope) {
 
 siatApp.controller('userHomeController', function ($scope) {
     $scope.Usuario = 'Usuario Ejemplo',
-    $scope.personalData = { Nombre: 'Usuario Ejemplo', Domicilio: 'Mariano Escobedo 124 Miguel Hidalgo DF' };
+            $scope.personalData = {Nombre: 'Usuario Ejemplo', Domicilio: 'Mariano Escobedo 124 Miguel Hidalgo DF'};
 });
 
 siatApp.controller('usergetTaxiController', function ($scope) {
@@ -68,13 +89,13 @@ siatApp.controller('usergetTaxiController', function ($scope) {
         Nombre: 'Usuario Ejemplo',
         Origen: 'Mariano Escobedo 124 Miguel Hidalgo DF',
         Destino: null
-    }
+    },
     $scope.getTaxiSuccess = function () {
         alert('Se ha solicitado el taxi, debe de aparecer en su puerta en menos de 15 minutos');
         window.location.href = '#/user';
-    }
-    $scope.cancelTaxi = function () {
-        alert('Se ha cancelado la operacion');
-        window.location.href = '#/user';
-    }
+    },
+            $scope.cancelTaxi = function () {
+                alert('Se ha cancelado la operacion');
+                window.location.href = '#/user';
+            };
 });
